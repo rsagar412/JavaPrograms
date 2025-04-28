c5824c763e716c87a1a9d274dfff3a10ebbc23ac
Sagar Rathod <rathod.sagar23@yahoo.com>
2025-04-28 19:47:00 +0530

    arraydequeue

---
+++ ArrayDequeueJava.java
@@ -0,0 +1,16 @@
+import java.lang.reflect.Array;
+import java.util.ArrayDeque;
+
+public class ArrayDequeueJava {
+    public static void main(String[] args) {
+        ArrayDeque<Integer> deque = new ArrayDeque<>();
+        // deque.add(1);
+        // deque.add(2);
+        // deque.add(3);
+        // System.out.println(deque);
+        // System.out.println(deque.getFirst()); // 1
+        System.out.println(deque.size());
+        // System.out.println(deque.getLast()); // 3
+        // System.out.println(deque.peekFirst()); // 1
+    }
+}
