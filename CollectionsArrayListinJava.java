8313a8de9c9409bedae7bab1aeb91a8c0a809018
Sagar Rathod <rathod.sagar23@yahoo.com>
2025-04-28 19:47:22 +0530

    arraylist

---
+++ CollectionsArrayListinJava.java
@@ -0,0 +1,43 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+public class CollectionsArrayListinJava {
+    public static void main(String[] args) {
+        // ArrayList<Integer> list = new ArrayList<Integer>();
+
+        // List<String> linkedList = new LinkedList<>();
+        // linkedList.add("Sagar");
+        // linkedList.add("Rahul");
+        // System.out.println(linkedList.get(1)); // Slower (traverses from head)
+
+        // List<String> arrayList = new ArrayList<>();
+        // arrayList.add("Sagar");
+        // arrayList.add("Rahul");
+        // System.out.println(arrayList.get(1)); // Fast access (indexing)
+
+
+//         ArrayList<Integer> list2 = new ArrayList<Integer>(5);
+//         list.add(12);
+//         list.add(3);
+//         list.add(4);
+//         list.add(1,5);
+
+//         list2.add(33);
+//         list2.add(44);
+//         list2.add(44);
+//         list2.add(44);
+//         list2.add(44);
+//         list2.add(44);
+// list.addAll(0,list2);    //addall will append the elements bydefault, however, we can specify the index in order to add the elements starting from a specific index.
+//    list.clear();
+//    System.out.println(list.contains(33)); 
+//    System.out.println(list.isEmpty());
+//    System.out.println(list.indexOf(44));
+//         for(int i=0;i<list.size();i++){
+//             System.out.println(list.get(i));
+//         }
+        // for(int i: list){
+        //     System.out.println(i);
+        // }
+    }
+}
